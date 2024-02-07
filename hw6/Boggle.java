37c4036f7eb54b72cc28aeb6c405fecca3d36dbb
刘振伟 <JenwayHere@Gmail.com>
2024-02-08 06:32:05 +0800

    [hw] 6 :>

--- hw6/Boggle.java
+++ hw6/Boggle.java
@@ -1,13 +1,13 @@
 import java.util.List;
 
 public class Boggle {
-    
+
     // File path of dictionary file
     static String dictPath = "words.txt";
 
     /**
      * Solves a Boggle puzzle.
-     * 
+     *
      * @param k The maximum number of words to return.
      * @param boardFilePath The file path to Boggle board file.
      * @return a list of words found in given Boggle board.
@@ -16,7 +16,37 @@ public class Boggle {
      *         have them in ascending alphabetical order.
      */
     public static List<String> solve(int k, String boardFilePath) {
-        // YOUR CODE HERE
+        validBoard(boardFilePath);
+        validDict();
+        validK(k);
+
+
+
         return null;
+    }
+
+    private static void validBoard(String boardFilePath) {
+        In boardFile = new In(boardFilePath);
+        String[] board = boardFile.readAllLines();
+        int row = board.length;
+        int col = board[0].length();
+        for (String s : board) {
+            if (s.length() != col) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private static void validDict() {
+        In dict = new In(dictPath);
+        if (!dict.exists()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validK(int k) {
+        if (k <= 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
